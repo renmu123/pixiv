17eb6d813c5826b6227038098d2f8262e47b38e8
renmu <1101022351@qq.com>
2017-05-06 19:50:18 +0800

    下载收藏文件

--- Pixiv.py
+++ Pixiv.py
@@ -5,6 +5,7 @@
 # 多线程
 # 模块化
 #保存的文件夹不存在退出
+#增加爬取个人收藏入口
 
 import requests
 from bs4 import BeautifulSoup
@@ -12,28 +13,28 @@ import http.cookiejar as cookielib
 import os
 import time
 
-def comfirm_path():
+def comfirm_path(save_txt):
     path = input('请输入要保存的地址：')
     if not os.path.exists(path):
         print('你输入的地址不存在，请重新输入')
         comfirm_path()
     else:
-        f = open('path_txt.txt', 'w')
+        f = open(save_txt, 'w')
         f.write(path)
         f.close()
     return path
 
 # 自定义输入保存路径
-def path_save():
-    if os.path.isfile('path_txt.txt'):
-        f = open('path_txt.txt', 'r')
+def path_save(save_txt):
+    if os.path.isfile(save_txt):
+        f = open(save_txt, 'r')
         path = f.read()
         f.close()
         return path
-
     else:
-        path = comfirm_path()
+        path = comfirm_path(save_txt)
         return path
+
 #登录
 def login():
     account = input('请输入账号：')
@@ -66,8 +67,6 @@ def get_post_key():
     # print(post_key)
     return post_key
 
-path = path_save() #自定义输入保存路径
-
 session = requests.session()
 #第一次登陆后加载保存在本地的cookies
 n = 0
@@ -79,17 +78,6 @@ try:
 except:
     print("Cookie 未能加载")
 
-pro_path = os.getcwd() #源文件运行地址
-os.chdir(path)
-now_time = time.strftime('%Y-%m-%d')
-if os.path.exists(now_time):
-    os.chdir(now_time)
-    # print('文件夹已存在，继续执行')
-else:
-    os.mkdir(now_time)
-    os.chdir(now_time)
-
-agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
 headers = {
     "Host": "accounts.pixiv.net",
     "Referer": "https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index",
@@ -97,24 +85,25 @@ headers = {
     'Origin': 'https://accounts.pixiv.net'
 }
 
-#主函数
-def main():
-    #每日热点
-    url = 'https://www.pixiv.net/ranking.php?mode=daily'
-    login = session.get(url)
-    num = 1 #计数
-    img_headers = {
+img_headers = {
         "Host": "i.pximg.net",
         "Referer": "https://www.pixiv.net",
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
     }
+
+
+def day():
+    #每日热点
+    url = 'https://www.pixiv.net/ranking.php?mode=daily'
+    login = session.get(url)
+    num = 1 #计数
     pages = BeautifulSoup(login.text, 'lxml').find('div', class_ = 'ranking-items adjust').find_all('div', class_ = '_layout-thumbnail')
 
     for page in pages:
         # img_url = id.find('img')['data-src'].replace('240x480', '600x600')
         # print(img_url)
         id = page.find('img')['data-id']
-        print('正在下载中%s/50' % num)
+        print('正在下载中%s/20' % num)
         num += 1
         url = 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + id  #id页面
         # print(url)
@@ -162,15 +151,102 @@ def main():
     print('下载完成')
     time.sleep(1000)
 
+def collection (col_page):
+    url = 'https://www.pixiv.net/bookmark.php?rest=show&type=illust_all&p=' + col_page
+    login = session.get(url)
+    # print(page.text)
+
+    num = 1  # 计数
+    pages = BeautifulSoup(login.text, 'lxml').find('div', class_= 'display_editable_works').find_all('div',class_= '_layout-thumbnail')
+    os.chdir(path)
+    for page in pages:
+        id = page.find('img')['data-id']
+        print('正在下载中%s/50' % num)
+        num += 1
+        url = 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + id  # id页面
+        # print(url)
+        try:
+            real_page = session.get(url)
+            real_page_img = BeautifulSoup(real_page.text, 'lxml').find_all('div', class_='wrapper')[-1].find('img')['data-src']  # 非漫画的地址
+            # print(real_page_img) #原图地址
+            pic = real_page_img[-4:]  # 扩展名
+            if os.path.isfile(id + pic):  # 如果文件存在就跳过
+                # print('文件已存在')
+                continue
+            img = session.get(real_page_img, headers=img_headers)
+            f = open(id + pic, 'ab')
+            f.write(img.content)
+            f.close()
+        except:
+            # 漫画套图
+            manga_url = url.replace('medium', 'manga')
+            manga_page = session.get(manga_url)
+            manga_page_imgs = BeautifulSoup(manga_page.text, 'lxml').find_all('img', class_='image ui-scroll-view')
+            page_number = 0
+            if os.path.exists(id):
+                os.chdir(id)
+                # print('文件夹已存在，继续执行')
+            else:
+                os.mkdir(id)
+                os.chdir(id)
+
+            for manga_page_img in manga_page_imgs:
+                page_number += 1
+                manga_page_img_url = manga_page_img['data-src']
+                # print(manga_page_img_url)
+                pic = manga_page_img_url[-4:]  # 扩展名
+                if os.path.isfile(id + '_' + str(page_number) + pic):  # 如果文件存在就跳过
+                    # print('    文件已存在')
+                    continue
+                img = session.get(manga_page_img_url, headers=img_headers)
+                f = open(id + '_' + str(page_number) + pic, 'ab')
+                f.write(img.content)
+                f.close()
+
+            os.chdir(path)
+            continue
+
+    print('一页下载完成啦')
 
 if __name__ == '__main__':
     if n == 1:
         print('你已经登录')
     else:
-        print('尚未登录')
+        print('你尚未登录')
         login()
 
-    main()
+
+    choice = input('''
+    如果要爬取每日图片请输入day,
+    如果要爬取你的收藏请输入collection  
+    ''')
+
+    if choice == 'day':
+        path = path_save('day_txt.txt')  # 自定义输入保存路径
+        pro_path = os.getcwd()  # 源文件运行地址
+        os.chdir(path)
+        now_time = time.strftime('%Y-%m-%d')
+        if os.path.exists(now_time):
+            os.chdir(now_time)
+            # print('文件夹已存在，继续执行')
+        else:
+            os.mkdir(now_time)
+            os.chdir(now_time)
+
+        day()
+    elif choice == 'collection':
+        path = path_save('con_txt.txt')  # 自定义输入保存路径
+        pro_path = os.getcwd()  # 源文件运行地址
+        os.chdir(path)
+
+        for col_page in range(1, 7):
+           collection(str(col_page))
+           # print('aa')
+
+
+   # day()
+
+    # collection()
 
 
 
